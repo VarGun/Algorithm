f12b604f24372744e107efff837fe477a89ae627
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2024-07-17 21:25:17 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 304 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -1,12 +1,10 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
+def solution(num):
+  for _ in range(len(d), num + 1):
+    d.append((d[-3] + d[-2] + d[-1])% 1000000009)
+  print((d[num - 1]) )
 
 d = [1,2,4,7]
 n = int(input())
 for _ in range(n):
   num = int(input())
-  for i in range(len(d), num + 1):
-    d.append((d[-3] + d[-2] + d[-1])% 1000000009)
-  # print('d[i] : ', d[i])
-  print((d[num - 1]) )
+  solution(num)
