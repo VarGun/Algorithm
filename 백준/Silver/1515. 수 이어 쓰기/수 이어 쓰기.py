d6ac97c7301bffd89cd7956466076cc2b9747274
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2024-08-16 22:30:00 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.py
@@ -3,13 +3,13 @@ input = sys.stdin.readline
 
 n = input().strip()
 i = 0
-c_num = 1
+c_num = 0
 while i < len(n):
+  c_num += 1
   for j in str(c_num):
     if(i >= len(n)):
       break
     if(j == str(n[i])):
       i += 1
-  c_num += 1
 
-print(c_num - 1)
+print(c_num)
