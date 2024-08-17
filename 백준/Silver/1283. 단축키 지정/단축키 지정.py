1d3913a3b5f11fd36c9a9a89b196d3c8d823c18f
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2024-08-17 20:12:59 +0900

    [Silver I] Title: 단축키 지정, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+def solution(arr):
+  cmd = []
+  # arr : [['New'], ['Open'], ['Save'], ['Save', 'As'], ['Save', 'All']]
+  for i in range(len(arr)):
+    # print('cmd : ', cmd )
+    flag = False
+    for j in range(len(arr[i])):
+      if(not arr[i][j][0].upper() in cmd):
+        cmd.append(arr[i][j][0].upper())
+        flag = True
+        arr[i][j] = "[" + arr[i][j][0] +  "]" + arr[i][j][1:]
+        # print('i : ', i)
+        print(' '.join(arr[i]))
+        break
+    if(flag):
+      continue
+    
+    for j in range(len(arr[i])):
+      for k in range(len(arr[i][j])):
+        if(not arr[i][j][k].upper() in cmd):
+          cmd.append(arr[i][j][k].upper())
+          flag = True
+          arr[i][j] = arr[i][j][0:k] + "[" + arr[i][j][k] + "]" + arr[i][j][k + 1:]
+          print(' '.join(arr[i]))
+          break
+      if(flag):
+        break
+    if(not flag):
+      print(' '.join(arr[i]))
+      
+if __name__ == "__main__":
+  n = int(input())
+  arr = []
+  for _ in range(n):
+    arr.append(input().rstrip().split())
+  # print(arr)
+  solution(arr)
