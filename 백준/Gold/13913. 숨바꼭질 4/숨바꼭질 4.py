6d2972fd114b0a1adfb07a927c1f8f4f70e1b092
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-03-21 15:06:04 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 652 ms, Memory: 103608 KB -BaekjoonHub

---
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+_max = 10 ** 6
+
+def bfs(num):
+  global k
+  q = deque()
+  q.append(num)
+  visited = [0 for _ in range(_max + 1)]
+  parent = [0 for _ in range(_max + 1)]
+  parent[num] = num
+  while q:
+    head = q.popleft()
+    if(head == k):
+      print(visited[head])
+      ans = []
+      for i in range(visited[head] + 1):
+        ans.append(head)
+        head = parent[head]
+      ans.reverse()
+      print(*ans)
+      return
+    next = [head - 1, head + 1, head * 2]
+    for i in next:
+      if(0<= i <= _max and visited[i] == 0):
+        q.append(i)
+        visited[i] = visited[head] + 1
+        parent[i] = head
+
+bfs(n)
