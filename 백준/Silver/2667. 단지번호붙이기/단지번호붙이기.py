0ef92188a0612997a9d0e3abb9a3b9f284bd1f87
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-07-11 15:35:29 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,50 +1,39 @@
 from collections import deque
-
 n = int(input())
-arr = [[] for _ in range(n)]
+arr = []
+for _ in range(n):
+  arr.append(list(input()))
 
-for i in range(n):
-  line = input()
-  for j in range(n):
-    arr[i].append(int(line[j]))
-
-dx = [1, -1, 0, 0]
+cnt = 0  # 단지 수
+sizes = []  # 단지 크기들
 dy = [0, 0, 1, -1]
-visited = [[0] * n for _ in range(n)]
-cnt_list = []
-num = 1
-num_cnt = 0
+dx = [1, -1, 0, 0]
+visited = [[False for _ in range(n)] for _ in range(n)]
 
-def bfs(y, x):
-  global num, num_cnt
-  q = deque([[y, x]])
-  visited[y][x] = num
-  
+def bfs(y, x, cnt):
+  global visited, sizes
+  q = deque()
+  q.append([y, x])
+  visited[y][x] = True
   while q:
     head = q.popleft()
     y = head[0]
     x = head[1]
-    num_cnt += 1
-
     for i in range(4):
-      nx = x + dx[i]
       ny = y + dy[i]
-
-      if(0 <= nx < n and 0 <= ny < n and arr[ny][nx] == 1 and visited[ny][nx] == 0):
+      nx = x + dx[i]
+      if (0 <= ny < n and 0 <= nx < n and not visited[ny][nx] and arr[ny][nx] == '1'):
         q.append([ny, nx])
-        visited[ny][nx] = num
-  
-  cnt_list.append([num, num_cnt])
-
+        visited[ny][nx] = True
+        cnt += 1
+  sizes.append(cnt)
 
 for i in range(n):
   for j in range(n):
-    if(arr[i][j] == 1 and visited[i][j] == 0):
-      num_cnt = 0
-      num += 1
-      bfs(i, j)
-
-print(num -  1)
-cnt_list.sort(key=lambda x : x[1])
-for i in range(num - 1):
-  print(cnt_list[i][1])
+    if (arr[i][j] == '1' and visited[i][j] == False):
+      cnt = 1
+      bfs(i, j, cnt)
+print(len(sizes))
+sizes.sort()
+for i in range(len(sizes)):
+  print(sizes[i])
