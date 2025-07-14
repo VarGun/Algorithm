b4d1a352d4a8fc88ea183ac849c6a7e0551322b6
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-07-14 14:26:20 +0900

    [Gold V] Title: 적록색약, Time: 72 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -7,32 +7,7 @@ visited_jm = [[0 for _ in range(n)] for _ in range(n)]
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-def bfs_jm(y, x, char):
-  global visited_jm
-  q = deque()
-  q.append([y, x])
-
-  while q:
-    head = q.popleft()
-    x = head[1]
-    y = head[0]
-
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if(0 <= nx < n and 0 <= ny < n and visited_jm[ny][nx] == 0):
-        if(char == 'R' or char == 'G'):
-          if(arr[ny][nx] == 'R' or arr[ny][nx] == 'G'):
-            q.append([ny, nx])
-            visited_jm[ny][nx] = 1
-        elif(char == 'B'):
-          if(arr[ny][nx] == 'B'):
-            q.append([ny, nx])
-            visited_jm[ny][nx] = 1
-
-
-def bfs(y, x, char):
-  global visited
+def bfs(y, x, char, visited, js):
   q = deque()
   q.append([y, x])
 
@@ -45,19 +20,29 @@ def bfs(y, x, char):
       nx = x + dx[i]
       ny = y + dy[i]
       if(0 <= nx < n and 0 <= ny < n and visited[ny][nx] == 0):
-        if(arr[ny][nx] == char):
-          q.append([ny, nx])
-          visited[ny][nx] = 1
+        if(js):
+          if(arr[ny][nx] == char):
+            q.append([ny, nx])
+            visited[ny][nx] = 1
+        else: 
+          if(char == 'R' or char == 'G'):
+            if(arr[ny][nx] == 'R' or arr[ny][nx] == 'G'):
+              q.append([ny, nx])
+              visited_jm[ny][nx] = 1
+          elif(char == 'B'):
+            if(arr[ny][nx] == 'B'):
+              q.append([ny, nx])
+              visited_jm[ny][nx] = 1
 
 cnt = 0
 cnt_jm = 0
 for i in range(n):
   for j in range(n):
     if(visited[i][j] == 0):
-      bfs(i, j, arr[i][j]) # 정상
+      bfs(i, j, arr[i][j], visited, True) # 정상
       cnt += 1
     if(visited_jm[i][j] == 0):
-      bfs_jm(i, j, arr[i][j])
+      bfs(i, j, arr[i][j], visited_jm, False)
       cnt_jm += 1
 
 print(cnt, cnt_jm)
