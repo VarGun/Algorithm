e4cfd3eec335de0dccb1453a89cfb319b4d9b84d
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-07-16 14:13:34 +0900

    [Gold V] Title: 트리, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,34 +1,34 @@
+n = int(input())
 
-n = int(input()) # 노드의 수
-s = input().split() # 각 노드의 부모 노드 번호
-m = int(input()) # 지울 노드 
+gun = list(map(int, input().split()))
+tree = [[] for _ in range(n)]
+is_deleted = []
+for i in range(n):
+  if (gun[i] != -1):
+    tree[gun[i]].append(i)
+visited = [False for _ in range(n)]
+target = int(input())
 
-cnt = 0
 
-tr = [[] for i in range(0,n)] # 각 인덱스가 부모노드이고 인덱스에 해당하는 배열에 자식 노드의 번호를 저장
+def delete_dfs(i):
+  tar = tree[i]
+  is_deleted.append(i)
+  for j in range(len(tar)):
+    delete_dfs(tar[j])
+  tree[i] = ['deleted']
 
-for i in range(0, n):
-    if(s[i] != "-1"): # -1이 아니면 (부모가 있으면)
-        tr[(int)(s[i])].append(i) # 부모노드 번호의 인덱스에 해당하는 배열에 자식 노드 번호 append
 
-def search(a): # a = 지울 노드의 번호
-    if(len(tr[a]) != 0): # 지울 노드의 자식을 담은 배열의 길이가 0이 아니면
-        for i in tr[a]:
-            search(i) # 자식이 없는 노드를 찾을 때 까지 재귀
-    tr[a] = ["gun"] # 찾으면 지워진 노드로 표시
+delete_dfs(target)
+ans = 0
+for i in range(len(tree)):
+  if (len(tree[i]) == 0):
+    ans += 1
+  elif (tree[i][0] != 'deleted'):
+    gugu = 0
+    for j in range(len(tree[i])):
+      if (len(tree[tree[i][j]]) != 0 and tree[tree[i][j]][0] == 'deleted'):
+        gugu += 1
+    if (gugu == len(tree[i])):
+      ans += 1
 
-search(m)
-
-for i in range(0, n):
-    if (len(tr[i]) == 0): # 자식 노드가 없으면
-        cnt += 1 # 리프노드의 수 + 1
-    elif((tr[i][0]) != 'gun'): # 지워진 노드가 아니면서 자식을 담은 배열의 길이가 0이 아니면
-        jud = 0 # 지워진 노드인지 판별할 수
-        for j in tr[i]:
-            if(len(tr[j]) == 1): # 배열의 길이가 1이면서
-                if(tr[j][0] == 'gun'): # 지워진 노드로 표시되어 있으면
-                    jud += 1 
-        if(jud == len(tr[i])): # 자식들을 담은 배열의 길이 = 지워진 노드로 표시되어 있는 자식들의 수 
-            cnt+=1 # 리프노드의 수 + 1
-
-print(cnt)
+print(ans)
