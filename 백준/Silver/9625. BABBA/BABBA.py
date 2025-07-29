63b83a15b0abf9a61707b8d195224be1303d087f
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-07-29 13:23:11 +0900

    [Silver V] Title: BABBA, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,8 @@
+K = int(input())
+
+dp = [[0, 0] for _ in range(K + 1)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+for i in range(2, K + 1):
+  dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
+print(dp[K][0], dp[K][1])
