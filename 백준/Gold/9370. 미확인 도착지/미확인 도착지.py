386f1b4af1298743ae7eef1d81b59c8e4374e3a5
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-08-06 14:31:20 +0900

    [Gold II] Title: 미확인 도착지, Time: 3432 ms, Memory: 46772 KB -BaekjoonHub

--- 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -8,44 +8,52 @@ ans = []
 
 def dij(s, graph):
   dis = [INF for _ in range(n + 1)]  # 거리
-
   dis[s] = 0
   heap = []
   heapq.heappush(heap, (0, s))
   while heap:
     weight, cur_node = heapq.heappop(heap)
-    if (dis[cur_node] < weight):
+    if dis[cur_node] < weight:
       continue
     for w, next_n in graph[cur_node]:
       next_w = w + weight
-      if (next_w < dis[next_n]):
+      if next_w < dis[next_n]:
         dis[next_n] = next_w
         path[next_n] = cur_node
         heapq.heappush(heap, (next_w, next_n))
   return dis
 
+
 while T:
   T -= 1
-  n, m, t = map(int, input().split())  # 노드 수, 간선 수, 목적지 후보 수
-  s, g, h = map(int, input().split())  # 시작점, 경로 양쪽 노드 둘
-  tar_list = [[g, h], [h, g]]
-  graph = [[] for _ in range(n + 1)]  # 교차로 = 노드
+  n, m, t = map(int, input().split())
+  s, g, h = map(int, input().split())
 
-  # 경로 [시작, 도착] 을 사용했는지
+  graph = [[] for _ in range(n + 1)]
+  gh_w = None
+
+
   path = [-1 for _ in range(n + 1)]
   path[s] = s
-  for i in range(m):
-    a, b, d = map(int, input().split())  # 양방향 + 가중치 추가
-    graph[a].append([d, b])
-    graph[b].append([d, a])
+
+  for _ in range(m):
+    a, b, d = map(int, input().split())
+    graph[a].append((d, b))
+    graph[b].append((d, a))
+
+    if (a == g and b == h) or (a == h and b == g):
+      gh_w = d
+
+
   cands = [int(input()) for _ in range(t)]
+
   dis_s = dij(s, graph)
-  ans = []
   dis_g = dij(g, graph)
   dis_h = dij(h, graph)
 
-  for i in range(len(cands)):
-    if (dis_s[g] + dis_g[h] + dis_h[cands[i]] == dis_s[cands[i]] or dis_s[h] + dis_h[g] + dis_g[cands[i]] == dis_s[cands[i]]):
-      ans.append(cands[i])
-  tmp = sorted(ans)
-  print(*tmp)
+  ans = []
+  for e in cands:
+    if (dis_s[g] + gh_w + dis_h[e] == dis_s[e]) or (dis_s[h] + gh_w + dis_g[e] == dis_s[e]):
+      ans.append(e)
+
+  print(*sorted(ans))
