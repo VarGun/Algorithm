2ccc021154d3b5084fad27effd2eeb6ec013bc07
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-09-29 15:14:30 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 8126.86 ms, Memory: 9.77 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.py
@@ -22,20 +22,22 @@ def solution(points, routes):
     ended = [False for _ in range(len(routes))] # 로봇들 끝났는지 체크
     
     # 처음 충돌 검사
+    current_set = set()
+    conflict_set = set()
     v_map = [[0 for _ in range(101)] for _ in range(101)] # 그 칸에 있는 로봇 수 -> 이미 있는 칸인지 검사용
     v_TF = [[False for _ in range(101)] for _ in range(101)] # 겹쳤는지 검사용
+    
     for i in range(len(routes)):
-        if(v_map[current[i][0]][current[i][1]] >= 1): # 이미 그 자리에 로봇이 있었으면
-            v_TF[current[i][0]][current[i][1]] = True # 겹침 체크
-
-        v_map[current[i][0]][current[i][1]] += 1 # 로봇 이동
+        if((current[i][0], current[i][1]) in current_set):
+            conflict_set.add((current[i][0], current[i][1]))
+        current_set.add((current[i][0], current[i][1]))
          # 충돌 검사
-    for i in range(len(v_TF)):
-        for j in range(len(v_TF[i])):
-            if(v_TF[i][j] == True):
-                print('충돌 i : ', i, ', j : ', j)
-
-        answer += sum(v_TF[i])
+    answer += len(conflict_set)
+    # for i in range(len(v_TF)):
+    #     answer += sum(v_TF[i])
+    print('current_set : ', current_set)
+    print('conflict_set : ', conflict_set)
+    print('answer : ', answer)
     
     
     cnt = 0
@@ -45,9 +47,6 @@ def solution(points, routes):
         
         v_map = [[0 for _ in range(101)] for _ in range(101)] # 그 칸에 있는 로봇 수 -> 이미 있는 칸인지 검사용
         v_TF = [[False for _ in range(101)] for _ in range(101)] # 겹쳤는지 검사용
-        # print('current : ', current)
-        # print('tar : ', tar)
-        # print('--------------')
         # 로봇 하나씩
         for i in range(len(routes)):
             # 끝난 애면 냅두기
@@ -70,20 +69,9 @@ def solution(points, routes):
                 if(tar_i[i] >= len(routes[i])): # 최종 목적지까지 다 끝났으면
                     ended[i] = True
                 else: # 최종 목적지 아니라면
-                    # 목적지 갱신
-                    # if(i == 0):
-                        # print('routes[tar_i[i]] : ', routes[tar_i[i]])
-                        # print('tar_i[i] : ', tar_i[i])
-                    # tar[i] = points[tar_i[i] - 1]
                     tar[i] = points[routes[i][tar_i[i]] - 1]
         # 충돌 검사
         for i in range(len(v_TF)):
-            # for j in range(len(v_TF[i])):
-            #     if(v_TF[i][j] == True):
-            #         print('충돌 i : ', i, ', j : ', j)
-            # if(sum(v_TF[i]) != 0):
-            #     print('gml')
-            
             answer += sum(v_TF[i])
         
         # 3개 다 끝났으면 break
