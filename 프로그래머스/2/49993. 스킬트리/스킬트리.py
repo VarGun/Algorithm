5b8fb9b32252d55267ed1e513a8aaab46196ea20
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-10-13 16:02:59 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -1,16 +1,11 @@
 from itertools import combinations
 
-def make_str(lst):
-    return ''.join(lst)
-
 def compare_skill(skill, tree): # 선행 스킬, 대상 스킬 트리
     sk_idx = 0
-    tr_idx = 0
     for i in range(len(tree)):
         if(tree[i] in skill): # 상관 있는 스킬일 경우
             if(skill[sk_idx] == tree[i]):
                 sk_idx += 1
-                tr_idx += 1
             else:
                 return False
     return True
