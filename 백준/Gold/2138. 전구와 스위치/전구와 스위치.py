98ab4cd6ebe4c14f5da4fbb64a86f28fb039df16
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-10-16 20:14:50 +0900

    [Gold IV] Title: 전구와 스위치, Time: 108 ms, Memory: 35544 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.py
@@ -1,55 +1,48 @@
 import sys
 input = sys.stdin.readline
 
-def solution(arr, re_arr, tar, n):
+N = int(input())
+
+
+def on_light(idx, cur):  # 전구 켜는 함수
+  if idx == 0:
+    cur[0] = 1 - cur[0]
+    cur[1] = 1 - cur[1]
+  elif idx == N - 1:
+    cur[N - 1] = 1 - cur[N - 1]
+    cur[N - 2] = 1 - cur[N - 2]
+  else:
+    for i in range(idx - 1, idx + 2):
+      cur[i] = 1 - cur[i]
+
+
+cur_off = list(map(int, input().strip()))  # 현재 상태
+tar = list(map(int, input().strip()))  # 목표 상태
+
+cur_on = cur_off[:]
+on_light(0, cur_on)  # 현재 상태(켰음)
+
+
+def on_or_off(cur):  # cur[i - 1] 이 tar[i - 1] 과 다르면 전구 켜기
+  global tar, N
   cnt = 0
-  re_cnt = 1
+  for i in range(1, N):
+    if tar[i - 1] != cur[i - 1]:
+      cnt += 1
+      on_light(i, cur)
+  return cnt if cur == tar else -1
 
-  for i in range(1, n):
-    if(arr[i - 1] != tar[i - 1]):
-      if(i != n - 1):
-        cnt += 1
-        arr[i - 1], arr[i], arr[i + 1] = abs(arr[i - 1] - 1), abs(arr[i] - 1), abs(arr[i + 1] - 1)
-      else:
-        cnt += 1
-        arr[i - 1], arr[i]= abs(arr[i - 1] - 1), abs(arr[i] - 1)
 
-    if(re_arr[i - 1] != tar[i - 1]):
-      if(i != n - 1):
-        re_cnt += 1
-        re_arr[i - 1], re_arr[i], re_arr[i + 1] = abs(re_arr[i - 1] - 1), abs(re_arr[i] - 1), abs(re_arr[i + 1] - 1)
-      else:
-        re_cnt += 1
-        re_arr[i - 1], re_arr[i] = abs(re_arr[i - 1] - 1), abs(re_arr[i] - 1)
-      
-  if(arr == tar and re_arr == tar):
-    print(min(cnt, re_cnt))
-    return
-  elif(arr == tar):
-    print(cnt)
-    return
-  elif(re_arr == tar):
-    print(re_cnt)
-    return
-  else:
-    print(-1)
-    return
+off_cnt = on_or_off(cur_off[:])
+res = on_or_off(cur_on)
 
-if __name__ == "__main__":
-  n = int(input())
-  arr = []
-  re_arr = []
-  tar = []
-  a = input()
-  b = input()
-  if(a == b):
-    print(0)
-  
-  else:
-    for i in range(n):
-      arr.append(int(a[i]))
-      re_arr.append(int(a[i]))
-    for i in range(n):
-      tar.append(int(b[i]))
-    re_arr[0], re_arr[1] = abs(re_arr[0] - 1), abs(re_arr[1] - 1)
-    solution(arr, re_arr, tar, n)
+on_cnt = res + 1 if res != -1 else -1  # 이미 한 번 켰으니까
+
+if on_cnt == -1 and off_cnt == -1:
+  print(-1)
+elif on_cnt == -1:
+  print(off_cnt)
+elif off_cnt == -1:
+  print(on_cnt)
+else:
+  print(min(on_cnt, off_cnt))
