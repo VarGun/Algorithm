c37fc6d58b8c260a3bbb951d453ea6acf36cd99d
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-10-17 23:53:17 +0900

    [level 2] Title: 연도 별 평균 미세먼지 농도 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
+++ 프로그래머스/2/284530. 연도 별 평균 미세먼지 농도 조회하기/연도 별 평균 미세먼지 농도 조회하기.sql
@@ -1,6 +1,47 @@
--- 코드를 작성해주세요
-select year(YM) as YEAR, round(avg(PM_VAL1), 2) as PM10, round(avg(PM_VAL2), 2) as 'PM2.5'
+select year(YM) as YEAR, round(avg(PM_VAL1), 2) as 'PM10', round(avg(PM_VAL2), 2) as 'PM2.5'
 from AIR_POLLUTION
 where LOCATION2 = '수원'
-group by year(YM)
-order by year(YM)
+group by YEAR
+order by YEAR
+
+
+
+# select *
+# from AIR_POLLUTION
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 작성해주세요
+# select year(YM) as YEAR, round(avg(PM_VAL1), 2) as PM10, round(avg(PM_VAL2), 2) as 'PM2.5'
+# from AIR_POLLUTION
+# where LOCATION2 = '수원'
+# group by year(YM)
+# order by year(YM)
