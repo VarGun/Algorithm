0ca3208be537da4e9d60b759f5206ea338ec635f
Heegun Kwak <88530565+VarGun@users.noreply.github.com>
2025-10-18 23:40:39 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -3,10 +3,11 @@
 # 보호 시작일을 조회하는 SQL문을 작성해주세요. 
 # 이때 결과는 보호 시작일 순으로 조회해야 합니다.
 
-
-SELECT i.NAME as NAME, i.DATETIME as DATETIME
-from ANIMAL_INS i
-left join ANIMAL_OUTS o on i.ANIMAL_ID = o.ANIMAL_ID
-where o.ANIMAL_ID is null
+select NAME, DATETIME
+from ANIMAL_INS
+where ANIMAL_ID not in (
+    select ANIMAL_ID
+    from ANIMAL_OUTS
+)
 order by DATETIME
 limit 3
